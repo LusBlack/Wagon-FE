c92b6bb34560d316c2cb3415169a6b6e079d8d13
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-10-25 23:44:06 +0100

    added token check

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import LandingView from '@/views/LandingView.vue'
+import LocationView from '@/views/LocationView.vue';
+import axios from "axios";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,9 +16,42 @@ const router = createRouter({
         path: '/landing',
         name: 'landing',
         component: LandingView
-    }
+    },
+    {
+        path: '/location',
+        name: 'location',
+        component: LocationView
+    },
 
   ]
 })
+
+router.beforeEach((to, from) => {
+    if(to.name === 'Login') {
+        return true
+    }
+    if(!localStorage.getItem('token')) {
+        return {
+            name: 'Login'
+        }
+    }
+
+   checkTokenAuthenticity()
+})
+
+const checkTokenAuthenticity = () => {
+    axios.get("http://localhost:8000/api/user", {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+    .then((response) => {})
+    .catch((error) => {
+        localStorage.removeItem('token')
+        router.push({
+            name: 'Login'
+        })
+    })
+}
 
 export default router
